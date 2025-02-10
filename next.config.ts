b6dd8091955a6c15b027cc74aa039fe95dad676f
Ritesh <ritsrivastava@gmail.com>
2025-02-10 13:13:09 +0100

    fix: disable default image optimization and update asset prefix in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,11 @@
 const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output: 'export',
-  basePath: isProd ? '/profile-ritesh-next' : ''
+  basePath: isProd ? '/profile-ritesh-next' : '',
+  images: {
+    unoptimized: true // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/profile-ritesh-next/' : ''
 };
 
 export default nextConfig;
