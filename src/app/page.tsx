e0de7feea642daee4424f777a8a695f537dde6a1
Ritesh <ritsrivastava@gmail.com>
2025-02-24 23:35:09 +0100

    fix: update environment variable handling and remove unused files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Image from 'next/image';
 
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? '';
+const basePath = process.env.BASE_PATH ?? '';
 
 const imgSrc = `${basePath}/images/img1.png`;
 
 export default function Home() {
   console.log('process.env.NODE_ENV:', process.env.NODE_ENV);
-  console.log('NEXT_PUBLIC_BASE_PATH:', process.env.NEXT_PUBLIC_BASE_PATH);
+  console.log('BASE_PATH:', process.env.BASE_PATH);
   return (
     <main className='flex flex-col items-center justify-center h-screen'>
       <h1 className='text-4xl font-bold'>Hello, World!</h1>
