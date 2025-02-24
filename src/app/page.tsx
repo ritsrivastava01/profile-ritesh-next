8688acbc918c7aed3035d529b213923ad526427f
Ritesh <ritsrivastava@gmail.com>
2025-02-24 23:18:56 +0100

    fix: set NEXT_PUBLIC_BASE_PATH in workflow and log NODE_ENV in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? '';
 const imgSrc = `${basePath}/images/img1.png`;
 
 export default function Home() {
-  console.log('process.env: ', process.env);
+  console.log('process.env.NODE_ENV:', process.env.NODE_ENV);
   console.log('NEXT_PUBLIC_BASE_PATH:', process.env.NEXT_PUBLIC_BASE_PATH);
   return (
     <main className='flex flex-col items-center justify-center h-screen'>
