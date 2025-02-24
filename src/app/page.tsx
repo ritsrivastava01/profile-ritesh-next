e8e3d20d51703762b5a71fd7800ff0acc05b47f7
Ritesh <ritsrivastava@gmail.com>
2025-02-24 22:12:28 +0100

    fix: refactor image source path handling in Home component based on environment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image';
-const imgSrc =
-  process.env.NODE_ENV === 'production'
-    ? '/images/img1.png'
-    : 'images/img1.png';
+const isProd = process.env.NODE_ENV === 'production';
+const basePath = isProd ? '' : '/';
+const imgSrc = `${basePath}images/img1.png`;
 
 export default function Home() {
+  console.log(imgSrc);
   return (
     <main className='flex flex-col items-center justify-center h-screen'>
       <h1 className='text-4xl font-bold'>Hello, World!</h1>
