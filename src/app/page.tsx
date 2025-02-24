7f324f0eea0aece3c555622cfd2531cfa396be4e
Ritesh <ritsrivastava@gmail.com>
2025-02-24 23:08:53 +0100

    fix: log entire process.env for better visibility in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? '';
 const imgSrc = `${basePath}/images/img1.png`;
 
 export default function Home() {
-  console.log('Base Path:', basePath);
+  console.log('process.env: ', process.env);
   console.log('NEXT_PUBLIC_BASE_PATH:', process.env.NEXT_PUBLIC_BASE_PATH);
   return (
     <main className='flex flex-col items-center justify-center h-screen'>
