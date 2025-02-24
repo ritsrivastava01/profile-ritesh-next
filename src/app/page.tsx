301788a2d314e2b1619f1858e8997d7efd6085b6
Ritesh <ritsrivastava@gmail.com>
2025-02-24 23:37:20 +0100

    fix: simplify basePath handling in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
 import Image from 'next/image';
 
-const basePath = process.env.BASE_PATH ?? '';
-
-const imgSrc = `${basePath}/images/img1.png`;
+const imgSrc = `${process.env.BASE_PATH}/images/img1.png`;
 
 export default function Home() {
-  console.log('process.env.NODE_ENV:', process.env.NODE_ENV);
-  console.log('BASE_PATH:', process.env.BASE_PATH);
   return (
     <main className='flex flex-col items-center justify-center h-screen'>
       <h1 className='text-4xl font-bold'>Hello, World!</h1>
