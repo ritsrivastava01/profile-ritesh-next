b85dbb1ff5849d60fa4673593f7b04912d7bdabc
Ritesh <ritsrivastava@gmail.com>
2025-02-25 11:25:10 +0100

    fix: add basePath and assetPrefix configurations in next.config.ts for BASE_PATH support

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 /**
  * @type {import('next').NextConfig}
  */
+// const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
-  output: process.env.BASE_PATH ? 'export' : undefined
+  output: process.env.BASE_PATH ? 'export' : undefined,
+  basePath: process.env.BASE_PATH ? '/profile-ritesh-next' : '',
+  assetPrefix: process.env.BASE_PATH ? '/profile-ritesh-next' : ''
 };
 
 export default nextConfig;
