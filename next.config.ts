706fe9c385034f26562873b4bf2e9d2de759d02d
Ritesh <ritsrivastava@gmail.com>
2025-02-25 11:30:38 +0100

    fix: remove unused production check and console log related to BASE_PATH

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 /**
  * @type {import('next').NextConfig}
  */
-// const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output: process.env.BASE_PATH ? 'export' : undefined
 };
