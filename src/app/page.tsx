ec5c4e2f33ed2d26b2fd7956fd9f00deb8d28438
Ritesh <ritsrivastava@gmail.com>
2025-03-29 00:26:32 +0100

    chore: add more page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-import Hero from '@/components/Hero';
 import { PageLayout } from '@/components/PageLayout';
-import Test from '@/components/test';
 import { getStaticPathWithBasePath } from '@/lib/getPath';
+import Hero from '@/ui/sections/Hero';
+import Portfolio from '@/ui/sections/Portfolio';
+import Skills from '@/ui/sections/Skills';
+import Timeline from '@/ui/sections/Timeline';
 import Image from 'next/image';
 
 const imgSrc = getStaticPathWithBasePath('/images/img1.png');
@@ -11,10 +13,20 @@ export default function Home() {
     <PageLayout>
       <div className="pointer-events-none fixed inset-0 z-30 transition duration-300 [background:radial-gradient(600px_at_493px_530px,rgba(29,78,216,0.15),transparent_80%)] lg:absolute"></div>
       <h1 className="text-4xl font-bold">Hello, World!</h1>
-      <Test />
 
       <Image src={`${imgSrc}`} alt="img" width="64" height="64" />
-      <Hero />
+      <section>
+        <Hero />
+      </section>
+      <section>
+        <Skills />
+      </section>
+      <section>
+        <Timeline />
+      </section>
+      <section>
+        <Portfolio />
+      </section>
     </PageLayout>
   );
 }
